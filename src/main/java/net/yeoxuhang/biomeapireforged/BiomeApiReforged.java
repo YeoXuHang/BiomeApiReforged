d3df86c2a9355d2e318fe09885c20de2df822026
YeoXuHang <xuhang.yeo@gmail.com>
2022-12-28 17:05:02 +0800

    Hmm

--- src/main/java/net/yeoxuhang/biomeapireforged/BiomeApiReforged.java
+++ src/main/java/net/yeoxuhang/biomeapireforged/BiomeApiReforged.java
@@ -1,25 +1,9 @@
 package net.yeoxuhang.biomeapireforged;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.yeoxuhang.biomeapireforged.example.ExampleBiomes;
-
-import static net.yeoxuhang.biomeapireforged.example.ExampleBiomes.BIOMES;
 
 @Mod(BiomeApiReforged.MODID)
 public class BiomeApiReforged {
     public static final String MODID = "biomeapireforged";
 
-    public BiomeApiReforged() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        BIOMES.register(modEventBus);
-        modEventBus.addListener(this::setup);
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-    private void setup(final FMLCommonSetupEvent event) {
-        event.enqueueWork(ExampleBiomes::generateBiomes);
-    }
 }
